71d7b745692201d9d03ba0f673d9e330303c36bc
Eva & Productos - Gabriel Villavicencio <gvillavicencio@peruapps.com.pe>
2022-08-29 19:14:09 -0500

    Agregar archivos de proyecto.

---
+++ Fibonacci/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Fibonacci.Controllers
+{
+    [ApiController]    
+    [Route("[controller]")]
+    public class HomeController : Controller
+    {        
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            int a = 0, b = 1, c = 0;
+            Console.Write("{0} {1}", a, b);
+            for (int i = 1; i < id; i++)
+            {
+                c = a + b;
+                Console.Write(" {0}", c);
+                a = b;
+                b = c;
+            }
+
+            return c.ToString();
+
+        }
+    }
+}
